66ed55f06a813053f0d8473ee209adf071ac0b3d
tell723 <terumu723@google.com>
2020-03-13 16:09:13 +0900

    Initial Commit

---
+++ RestaurantSearcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantSearcher
+//
+//  Created by 渡邊輝夢 on 2020/03/13.
+//  Copyright © 2020 Terumu Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
